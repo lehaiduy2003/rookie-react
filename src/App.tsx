1494ad404833515777746ed48056db8275d41cfd
Le Hai Duy <lehaiduy2003@gmail.com>
2025-04-20 14:25:54 +0700

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <div className="">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <p className="text-lg">This is a simple React app with Tailwind CSS.</p>
+        <p className="text-sm text-gray-500">Enjoy building your application!</p>
+        <p className="text-base text-blue-500">This is a base text with blue color.</p>
+        <p className="text-red-500">This is a red text.</p>
+        <p className="text-green-500">This is a green text.</p>
+        <p className=""></p>
+      </div>
+    </>
+  );
+}
+
+export default App;

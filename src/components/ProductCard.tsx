faf7c8dc8bac17bc6b3bb4e560939e0ea99ad30b
Le Hai Duy <lehaiduy2003@gmail.com>
2025-04-23 01:22:49 +0700

    feat: handle text too long for using truncate

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,8 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { HeartIcon } from "lucide-react";
-import { IoStar } from "react-icons/io5";
 import { Card, CardContent } from "@/components/ui/card";
 import { Product } from "@/types/Product";
+import Rating from "./Rating";
 
 interface ProductCardProps {
   product: Product;
@@ -27,23 +27,21 @@ export default function ProductCard({ product }: ProductCardProps) {
       </figure>
       <CardContent className="px-4 pb-4">
         <div className="flex justify-between">
-          <div>
+          {/* Add width constraint for truncate the text */}
+          <div className="w-[65%]">
             <h3 className="text-lg">
-              <a href={`/products/${product.id}`} className="font-semibold text-primary">
+              <a
+                href={`/products/${product.id}`}
+                className="font-semibold text-primary truncate block overflow-hidden whitespace-nowrap"
+                title={product.name} /* Show full name on hover */
+              >
                 <span aria-hidden="true" className="absolute inset-0" />
                 {product.name}
               </a>
             </h3>
-            {product.ratingCount > 0 ? (
-              <p className="text-sm text-muted-foreground flex items-center">
-                {product.avgRating} <IoStar className="inline-block text-yellow-500" />
-                {` - ${product.ratingCount} ratings`}
-              </p>
-            ) : (
-              <p className="text-sm text-muted-foreground italic">No ratings yet</p>
-            )}
+            <Rating avgRating={product.avgRating} ratingCount={product.ratingCount} />
           </div>
-          <p className="text-lg font-semibold">{product.price}</p>
+          <p className="text-lg font-semibold">${product.price.toFixed(2)}</p>
         </div>
       </CardContent>
     </Card>
